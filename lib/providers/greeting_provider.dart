0872c2b88c9490123b92ef16b17523251f484df2
GakuruAlex <gakurualexgicimo@gmail.com>
2026-03-15 02:23:04 +0300

    Show loading when retrying after an error

--- lib/providers/greeting_provider.dart
+++ lib/providers/greeting_provider.dart
@@ -4,9 +4,9 @@ import 'package:riverpod_state_management/services/fake_api.dart';
 final fakeApiProvider = Provider((_) => FakeApiService());
 
 final greetingFutureProvider = FutureProvider((Ref ref) async {
-  ref.keepAlive();
   print("Provider executed");
+
   final fakeService = ref.read(fakeApiProvider);
 
-  return fakeService.getGreeting();
+  return await fakeService.getGreeting();
 });
